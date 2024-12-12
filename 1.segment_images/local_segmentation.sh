575202b3b14d0af55531ddfb1f034148b1568e8f
MikeLippincott <1michaell2017@gmail.com>
2024-12-11 21:10:51 -0700

    run all images

--- 1.segment_images/local_segmentation.sh
+++ 1.segment_images/local_segmentation.sh
@@ -22,15 +22,22 @@ if [ "$NOTEBOOK" = False ]; then
     z_stack_dir="../../data/z-stack_images/"
     input_dirs=$(ls -d $z_stack_dir*)
 
+
+    total_dirs=$(echo "$input_dirs" | wc -l)
+    current_dir=0
+
     # loop through all input directories
     for dir in $input_dirs; do
         dir=${dir%*/}
+        current_dir=$((current_dir + 1))
+        echo -ne "Processing directory $current_dir of $total_dirs\r"
         python 0.segment_nuclei_organoids.py --input_dir "$dir" --window_size 3 --clip_limit 0.05
         python 1.segment_cells_organoids.py --input_dir "$dir" --window_size 3 --clip_limit 0.1
         python 2.make_nuclei_segmentation_videos.py --input_dir "$dir"
         python 3.make_cell_segmentation_videos.py --input_dir "$dir"
     done
-        cd ../ || exit
+    echo -ne "\n"
+    cd ../ || exit
 fi
 
 # deactivate cellprofiler environment
