ae1e6cb022f449401ba6f22f87781dc8e3b5487e
MikeLippincott <1michaell2017@gmail.com>
2025-04-25 09:15:11 -0600

    ingest data

--- 0.preprocessing_data/scripts/0.update_file_structure.py
+++ 0.preprocessing_data/scripts/0.update_file_structure.py
@@ -55,43 +55,45 @@ else:
     # raw_image_dir_local = pathlib.Path("/media/18tbdrive/GFF_organoid_data/Cell Painting-NF0014 Thawed3-Pilot Drug Screening")
 
 
-# In[4]:
+# In[ ]:
 
 
 # Define parent and destination directories in a single dictionary
 dir_mapping = {
-    # "NF0014": {
-    #     "parent": pathlib.Path(
-    #         f"{raw_image_dir_local}/NF0014-Thawed 3 (Raw image files)-Combined/NF0014-Thawed 3 (Raw image files)-Combined copy"
-    #         if not HPC
-    #         else f"{raw_image_dir_hpc}/NF0014-Thawed 3 (Raw image files)-Combined/NF0014-Thawed 3 (Raw image files)-Combined copy"
-    #     ).resolve(strict=True),
-    #     "destination": pathlib.Path("../../data/NF0014/raw_images").resolve(),
-    # },
-    # "NF0016": {
-    #     "parent": pathlib.Path(
-    #         f"{raw_image_dir_local}/NF0016 Cell Painting-Pilot Drug Screening-selected/NF0016-Cell Painting Images/NF0016-images copy"
-    #         if not HPC
-    #         else f"{raw_image_dir_hpc}/NF0016 Cell Painting-Pilot Drug Screening-selected/NF0016-Cell Painting Images/NF0016-images copy"
-    #     ).resolve(strict=True),
-    #     "destination": pathlib.Path("../../data/NF0016/raw_images").resolve(),
-    # },
-    # "NF0017": {
-    #     "parent": pathlib.Path(
-    #         f"{raw_image_dir_local}/NF0017-T3-P7 (AGP, Mito Parameter optimization)/Acquisition 03-07-2025"
-    #         if not HPC
-    #         else f"{raw_image_dir_hpc}/NF0017-T3-P7 (AGP, Mito Parameter optimization)/Acquisition 03-07-2025" # TODO: Update this later if not correct
-    #     ).resolve(strict=True),
-    #     "destination": pathlib.Path("../../data/raw_images/NF0017/raw_images").resolve(),
-    # },
-    # "NF0018": {
-    #     "parent": pathlib.Path(
-    #         f"{raw_image_dir_local}/NF0018 (T6) Cell Painting-Pilot Drug Screeining/NF0018-Cell Painting Images/NF0018-All Acquisitions"
-    #         if not HPC
-    #         else f"{raw_image_dir_hpc}/NF0018 (T6) Cell Painting-Pilot Drug Screeining/NF0018-Cell Painting Images/NF0018-All Acquisitions"
-    #     ).resolve(strict=True),
-    #     "destination": pathlib.Path("../../data/NF0018/raw_images").resolve(),
-    # },
+    "NF0014": {
+        "parent": pathlib.Path(
+            f"{raw_image_dir_local}/NF0014-Thawed 3 (Raw image files)-Combined/NF0014-Thawed 3 (Raw image files)-Combined copy"
+            if not HPC
+            else f"{raw_image_dir_hpc}/NF0014-Thawed 3 (Raw image files)-Combined/NF0014-Thawed 3 (Raw image files)-Combined copy"
+        ).resolve(strict=True),
+        "destination": pathlib.Path("../../data/NF0014/raw_images").resolve(),
+    },
+    "NF0016": {
+        "parent": pathlib.Path(
+            f"{raw_image_dir_local}/NF0016 Cell Painting-Pilot Drug Screening-selected/NF0016-Cell Painting Images/NF0016-images copy"
+            if not HPC
+            else f"{raw_image_dir_hpc}/NF0016 Cell Painting-Pilot Drug Screening-selected/NF0016-Cell Painting Images/NF0016-images copy"
+        ).resolve(strict=True),
+        "destination": pathlib.Path("../../data/NF0016/raw_images").resolve(),
+    },
+    "NF0017": {
+        "parent": pathlib.Path(
+            f"{raw_image_dir_local}/NF0017-T3-P7 (AGP, Mito Parameter optimization)/Acquisition 03-07-2025"
+            if not HPC
+            else f"{raw_image_dir_hpc}/NF0017-T3-P7 (AGP, Mito Parameter optimization)/Acquisition 03-07-2025"  # TODO: Update this later if not correct
+        ).resolve(strict=True),
+        "destination": pathlib.Path(
+            "../../data/raw_images/NF0017/raw_images"
+        ).resolve(),
+    },
+    "NF0018": {
+        "parent": pathlib.Path(
+            f"{raw_image_dir_local}/NF0018 (T6) Cell Painting-Pilot Drug Screeining/NF0018-Cell Painting Images/NF0018-All Acquisitions"
+            if not HPC
+            else f"{raw_image_dir_hpc}/NF0018 (T6) Cell Painting-Pilot Drug Screeining/NF0018-Cell Painting Images/NF0018-All Acquisitions"
+        ).resolve(strict=True),
+        "destination": pathlib.Path("../../data/NF0018/raw_images").resolve(),
+    },
     "NF0021": {
         "parent": pathlib.Path(
             f"{raw_image_dir_local}/NF0021-T1/NF0021-T1 Combined"
