2012472b377f3afc7e1f3f4c210c6a81446e39f7
MikeLippincott <1michaell2017@gmail.com>
2025-04-25 09:14:20 -0600

    ingest data

--- 0.preprocessing_data/scripts/1.make_z-stack_images.py
+++ 0.preprocessing_data/scripts/1.make_z-stack_images.py
@@ -9,10 +9,22 @@
 
 
 import pathlib
+import pprint
 
 import numpy as np
 import tifffile as tiff
-import tqdm
+
+# check if in a jupyter notebook
+try:
+    cfg = get_ipython().config
+    in_notebook = True
+except NameError:
+    in_notebook = False
+
+if in_notebook:
+    from tqdm.notebook import tqdm
+else:
+    import tqdm
 
 
 # ## Set input and output directories
@@ -20,86 +32,108 @@ import tqdm
 # In[2]:
 
 
-input_dir = pathlib.Path("../../data/NF0014_raw_images").resolve(strict=True)
+list_of_patients = [  # will be in a separate file in the future
+    # "NF0014",
+    # "NF0016",
+    # "NF0017",
+    "NF0018",
+    "NF0021",
+    "SACRO219",
+]
 
-output_z_stack_dir = pathlib.Path("../../data/NF0014_zstack_images").resolve()
-output_z_stack_dir.mkdir(exist_ok=True, parents=True)
-
-
-# ## Create list of the well-site folders
 
 # In[3]:
 
 
-# get a list of all dirs in the input dir
-input_dirs = [x for x in input_dir.iterdir() if x.is_dir()]
-input_dirs.sort()
-print(f"There are {len(input_dirs)} directories in the input directory.")
+patient_input_dict = {}
+for patient in list_of_patients:
+    patient_input_dict[patient] = {
+        "raw_images": pathlib.Path(f"../../data/{patient}/raw_images").resolve(),
+        "zstack_output": pathlib.Path(f"../../data/{patient}/zstack_images").resolve(),
+    }
+pprint.pprint(patient_input_dict)
 
-
-# ## For the NF0014 dataset, remove the files for one of the z-slices due to missing channel (640)
 
 # In[4]:
 
 
-# Remove files containing 'ZS000' (first z-slice) in the F11-3 directory because it is missing 640 channel
-for file in pathlib.Path(f'{input_dir}/F11-3').glob('*ZS000*'):
-    file.unlink()
-    print(f"Removed: {file}")
+# Image extensions that we are looking to copy
+image_extensions = {".tif", ".tiff"}
 
-
-# ## Set the dictionary for filenames and filepaths
 
 # In[5]:
+
+
+unlisted_images = {"patient": ["NF0014"], "image_set": ["F11-3"]}
+
+
+# In[6]:
 
 
 image_extensions = {".tif", ".tiff"}
 channel_names = ["405", "488", "555", "640", "TRANS"]
 # make a dictionary that contains a list for each channel name, storing both filepath and filename
-channel_images = {channel_name: {"filename": [], "filepath": []} for channel_name in channel_names}
+channel_images = {
+    channel_name: {"filename": [], "filepath": []} for channel_name in channel_names
+}
 channel_images
 
 
-# ## Loop thorugh and create z-stack images for each FOV of each well in their respective directories.
+# ## Create list of the well-site folders
 
-# In[6]:
+# In[7]:
 
 
-for well_dir in tqdm.tqdm(input_dirs):
-    print(f"Processing well_dir: {well_dir.stem}")  # Debug
-    channel_images = {channel_name: {"filepath": []} for channel_name in channel_names}
-    
-    # Get all the images in the directory
-    for filename in well_dir.glob("*"):
-        if filename.suffix in image_extensions:
-            for channel_name in channel_names:
-                if channel_name in filename.name:
-                    channel_images[channel_name]["filepath"].append(filename)
-                    break
+for patient in tqdm(patient_input_dict.keys(), desc="Processing patients"):
+    input_dirs = [
+        x for x in patient_input_dict[patient]["raw_images"].iterdir() if x.is_dir()
+    ]
+    input_dirs.sort()
 
-    # Iterate through each channel
-    for channel_name in channel_names:
-        # Sort and check filepaths
-        channel_images[channel_name]["filepath"] = sorted(
-            channel_images[channel_name]["filepath"]
-        )
-        if not channel_images[channel_name]["filepath"]:
-            print(f"No files found for channel {channel_name} in {well_dir}. Skipping...")
+    for well_dir in tqdm(
+        input_dirs, desc=f"Processing wells for patient {patient}", leave=False
+    ):
+        channel_images = {
+            channel_name: {"filename": [], "filepath": []}
+            for channel_name in channel_names
+        }
+        if (
+            patient in unlisted_images["patient"]
+            and well_dir.name in unlisted_images["image_set"]
+        ):
+            print(f"Skipping {well_dir.name} because it is not listed.")
             continue
 
-        # Confirm before accessing
-        print(f"Stacking channel {channel_name}")  # Debug
-    
-        # read the image files from the sorted file paths and stack them into a numpy array
-        images_to_stack = np.array(
-            [
-                tiff.imread(filepath)
-                for filepath in channel_images[channel_name]["filepath"]
-            ]
-        )
-        filepath = channel_images[channel_name]["filepath"][0]
-        well = str(filepath.parent).split("/")[-1]
-        output_path = output_z_stack_dir / f"{well}" / f"{well}_{channel_name}.tif"
-        output_path.parent.mkdir(exist_ok=True, parents=True)
-        tiff.imwrite(output_path, images_to_stack)
+        for filename in well_dir.glob("*"):
+            if filename.suffix in image_extensions:
+                for channel_name in channel_names:
+                    if channel_name in filename.name:
+                        channel_images[channel_name]["filepath"].append(filename)
 
+        for channel_name in tqdm(
+            channel_names, desc=f"Processing channels in {well_dir.name}", leave=False
+        ):
+            channel_images[channel_name]["filepath"] = sorted(
+                channel_images[channel_name]["filepath"]
+            )
+            if not channel_images[channel_name]["filepath"]:
+                print(
+                    f"No files found for channel {channel_name} in {well_dir}. Skipping..."
+                )
+                continue
+
+            images_to_stack = np.array(
+                [
+                    tiff.imread(filepath)
+                    for filepath in channel_images[channel_name]["filepath"]
+                ]
+            )
+            filepath = channel_images[channel_name]["filepath"][0]
+            well = str(filepath.parent).split("/")[-1]
+            output_path = (
+                patient_input_dict[patient]["zstack_output"]
+                / f"{well}"
+                / f"{well}_{channel_name}.tif"
+            )
+            output_path.parent.mkdir(exist_ok=True, parents=True)
+            tiff.imwrite(output_path, images_to_stack)
