1c670b3c3d2682379b204e580e0a23afcd887e90
MikeLippincott <1michaell2017@gmail.com>
2025-04-30 09:03:03 -0600

    run on HPC

--- 2.segment_images/clean_up_segmentation_intermediates.sh
+++ 2.segment_images/clean_up_segmentation_intermediates.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=6:00:00
-#SBATCH --output=segmentation_cleanup-%j.out
+#SBATCH --time=1:00:00
+#SBATCH --output=segmentation_grandparent-%j.out
 
+# activate  cellprofiler environment
 module load anaconda
 conda init bash
 conda activate GFF_segmentation
 
-echo "Cleaning up segmentation files"
-python 7.clean_up_segmentation.py >> segmentation.log
-echo -ne "\n"
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
+
+patient_array=( "NF0014" "NF0016" "NF0018" "NF0021" "SARCO219" )
+
+for patient in "${patient_array[@]}"; do
+    number_of_jobs=$(squeue -u $USER | wc -l)
+    while [ $number_of_jobs -gt 990 ]; do
+        sleep 1s
+        number_of_jobs=$(squeue -u $USER | wc -l)
+    done
+    echo "Cleaning up segmentation files for patient: $patient"
+    python 7.clean_up_segmentation.py --patient "$patient"
+
+done
 
 conda deactivate
 echo "Segmentation cleanup completed"
