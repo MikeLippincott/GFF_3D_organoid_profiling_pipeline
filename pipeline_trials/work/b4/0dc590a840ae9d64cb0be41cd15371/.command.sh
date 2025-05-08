9fbffd19292cdce7025f1186781513e8f061545b
MikeLippincott <1michaell2017@gmail.com>
2025-05-08 10:43:03 -0600

    feature walkthrough

--- pipeline_trials/work/b4/0dc590a840ae9d64cb0be41cd15371/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/../3.cellprofiling/slurm_scripts/ || exit 1
-echo "Processing patient: NF0014, well_fov: C4-2"
-bash run_colocalization_child.sh NF0014 FALSE C4-2
-cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/ || exit 1
