53a06e7bc053154c938b0a7ea93f667081afe1f8
MikeLippincott <1michaell2017@gmail.com>
2025-05-08 10:42:39 -0600

    feature walkthrough

---
+++ pipeline_trials/work/30/136964606191678a5a06c275b4f490/.command.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ue
+cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/../3.cellprofiling/slurm_scripts/ || exit 1
+echo "Processing patient: NF0014, well_fov: C4-2"
+bash run_granularity_child.sh NF0014 TRUE C4-2
+cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/ || exit 1
---
+++ pipeline_trials/work/b4/34c49d0e6139b9962635534a4d2a3a/.command.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ue
+cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/../3.cellprofiling/slurm_scripts/ || exit 1
+echo "Processing patient: NF0014, well_fov: C4-2, use_gpu: /home/lippincm/miniforge3/envs/"
+bash run_area_shape_child.sh NF0014 C4-2 TRUE /home/lippincm/miniforge3/envs/
+cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/ || exit 1
