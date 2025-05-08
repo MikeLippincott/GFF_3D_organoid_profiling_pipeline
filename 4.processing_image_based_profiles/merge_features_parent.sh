70d732ef74e8d9fbc11aa35ee9e801b065e14f59
MikeLippincott <1michaell2017@gmail.com>
2025-05-08 10:14:54 -0600

    merge the profiles

--- 4.processing_image_based_profiles/merge_features_parent.sh
+++ 4.processing_image_based_profiles/merge_features_parent.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-
 module load anaconda
 conda init bash
-conda activate GFF_featurization
-
-jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
+conda activate nf1_image_based_profiling_env
 
 
-USE_GPU="FALSE"
 patient=$1
 
-parent_dir="../data/$patient/cellprofiler"
+parent_dir="../data/$patient/extracted_features"
 # get the list of all dirs in the parent_dir
 dirs=$(ls -d $parent_dir/*)
+
+cd scripts/ || exit
+python get_profiling_stats.py --patient "$patient"
+cd ../ || exit
 
 
 # loop through each dir and submit a job
@@ -33,12 +33,12 @@ for dir in $dirs; do
         --partition=amilan \
         --qos=long \
         --account=amc-general \
-        --time=7-00:00:00 \
+        --time=1:00:00 \
         --output=parent_featurize-%j.out \
-        HPC_run_featurization_parent.sh "$well_fov" $USE_GPU $patient
+        merge_features_parent.sh "$well_fov" "$patient"
 
 done
 
 conda deactivate
 
-echo "Featurization done"
+echo "All well_fov submitted for patient $patient"
