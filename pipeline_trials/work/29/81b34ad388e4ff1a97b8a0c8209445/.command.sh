1bb88c98c6775b3e267b7b4e7b57a96498d19bd8
MikeLippincott <1michaell2017@gmail.com>
2025-05-08 10:43:03 -0600

    feature walkthrough

--- pipeline_trials/work/29/81b34ad388e4ff1a97b8a0c8209445/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/../2.segment_images/ || exit 1
-echo "Processing patient: NF0014, well_fov: C4-2"
-source child_segmentation.sh C4-2 NF0014
-cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/ || exit 1
--- pipeline_trials/work/6d/ef0b99104251691afbc5348aebe6c9/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/../3.cellprofiling/slurm_scripts/ || exit 1
-echo "Running CPU featurization for patient: NF0014, well_fov: C4-2 use_gpu: false"
-bash run_texture_child.sh C4-2 FALSE NF0014
-cd /home/lippincm/Documents/GFF_3D_organoid_profiling_pipeline/pipeline_trials/ || exit 1
