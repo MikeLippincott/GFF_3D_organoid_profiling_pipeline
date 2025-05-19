c549c33e767eb78358b87e59c68835069d09b095
MikeLippincott <1michaell2017@gmail.com>
2025-05-19 13:06:17 -0600

    process new patient

--- 0.preprocessing_data/HPC_preprocessing.sh
+++ 0.preprocessing_data/HPC_preprocessing.sh
@@ -15,8 +15,8 @@ jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*
 
 cd scripts/ || exit
 
-python 0.patient_specific_preprocessing.py --HPC True
-python 1.update_file_structure.py --HPC True
+# python 0.patient_specific_preprocessing.py --HPC
+# python 1.update_file_structure.py --HPC
 python 2.make_z-stack_images.py
 
 cd .. || exit
