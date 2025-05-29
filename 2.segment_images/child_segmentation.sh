b5cb78c441bc016eefbaf7df73d3cb630e734195
MikeLippincott <1michaell2017@gmail.com>
2025-05-28 22:23:09 -0600

    need to clean up + rerun

--- 2.segment_images/child_segmentation.sh
+++ 2.segment_images/child_segmentation.sh
@@ -14,25 +14,69 @@ cd scripts/ || exit
 well_fov=$1
 patient=$2
 echo "Processing well_fov $well_fov for patient $patient"
-compartments=( "nuclei" "cell" "organoid" )
+compartments=( "nuclei" "organoid" ) # we do not do 2.5D segmentation for cells in this script
 
-# echo "Reslicing images" # comment out for now -> needs further testing
-# python 00.reslice_images.py --well_fov "$well_fov"
-echo "Segmenting Nuclei"
-python 0.segment_nuclei_organoids.py --patient "$patient" --well_fov "$well_fov" --window_size 2 --clip_limit 0.05 >> segmentation.log
-echo "Completed Nuclei Segmentation"
-echo "Segmenting Cells"
-python 1.segment_cells_organoids.py --patient "$patient" --well_fov "$well_fov" --window_size 3 --clip_limit 0.1 >> segmentation.log
-echo "Completed Cell Segmentation"
-echo "Segmenting Organoids"
-python 2.segment_whole_organoids.py --patient "$patient" --well_fov "$well_fov" --window_size 4 --clip_limit 0.1 >> segmentation.log
-echo "Completed Organoid Segmentation"
+
+python 0.segment_nuclei_organoids.py \
+    --patient "$patient" \
+    --well_fov "$well_fov" \
+    --window_size 3 \
+    --clip_limit 0.05
+
+python 2.segment_whole_organoids.py \
+    --patient "$patient" \
+    --well_fov "$well_fov" \
+    --window_size 4 \
+    --clip_limit 0.1
+
+conda deactivate
+conda activate GFF_segmentation_cellpose4
+python 1.segment_cells_watershed_method.ipynb \
+    --patient "$patient" \
+    --well_fov "$well_fov" \
+    --clip_limit 0.05
+
+
 for compartment in "${compartments[@]}"; do
-    echo "Decoupling $compartment"
-    python 3.segmentation_decoupling.py --patient "$patient" --well_fov "$well_fov" --compartment "$compartment" >> segmentation.log
-    python 4.reconstruct_3D_masks.py --patient "$patient" --well_fov "$well_fov" --compartment "$compartment" --radius_constraint 10 >> segmentation.log
+
+    if [ "$compartment" == "nuclei" ]; then
+        window_size=3
+    elif [ "$compartment" == "organoid" ]; then
+        window_size=4
+    else
+        echo "Not specified compartment: $compartment"
+
+    fi
+    python 3.segmentation_decoupling.py \
+        --patient "$patient" \
+        --well_fov "$well_fov" \
+        --compartment "$compartment" \
+        --window_size "$window_size"
+
+    python 4.reconstruct_3D_masks_copy.py \
+        --patient "$patient" \
+        --well_fov "$well_fov" \
+        --compartment "$compartment"
+
+    python 5.post-hoc_mask_refinement.py \
+        --patient "$patient" \
+        --well_fov "$well_fov" \
+        --compartment "$compartment"
 done
-python 5.create_cytoplasm_masks.py --patient "$patient" --well_fov "$well_fov" >> segmentation.log
+
+python 5.post-hoc_mask_refinement.py \
+    --patient "$patient" \
+    --well_fov "$well_fov" \
+    --compartment "cell"
+
+python 6.post_hoc_reassignment.py \
+    --patient "$patient" \
+    --well_fov "$well_fov"
+
+python 7.create_cytoplasm_masks.py \
+    --patient "$patient" \
+    --well_fov "$well_fov"
+
 
 cd ../ || exit
 
