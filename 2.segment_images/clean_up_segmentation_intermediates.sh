660b0911543d478b2d63a86d75cea194a6074e7c
MikeLippincott <1michaell2017@gmail.com>
2025-06-05 22:08:07 -0600

    update nf config

--- 2.segment_images/clean_up_segmentation_intermediates.sh
+++ 2.segment_images/clean_up_segmentation_intermediates.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=1:00:00
+#SBATCH --time=6:00:00
 #SBATCH --output=segmentation_grandparent-%j.out
 
 # activate  cellprofiler environment
@@ -14,7 +14,7 @@ conda activate GFF_segmentation
 
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
-patient_array=( "NF0014" "NF0016" "NF0018" "NF0021" "SACRO219" )
+patient_array=( "NF0014" "NF0016" "NF0018" "NF0021" "SACRO219" "SARCO361" )
 
 cd scripts/ || exit
 
@@ -25,7 +25,7 @@ for patient in "${patient_array[@]}"; do
         number_of_jobs=$(squeue -u $USER | wc -l)
     done
     echo "Cleaning up segmentation files for patient: $patient"
-    python 7.clean_up_segmentation.py --patient "$patient"
+    python 9.clean_up_segmentation.py --patient "$patient"
 
 done
 
