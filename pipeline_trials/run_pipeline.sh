743cbe879640e132a90861864aa6b549ef9b9d12
MikeLippincott <1michaell2017@gmail.com>
2025-06-05 20:38:26 -0600

    adjusting pipe

--- pipeline_trials/run_pipeline.sh
+++ pipeline_trials/run_pipeline.sh
@@ -2,9 +2,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=amilan
-#SBATCH --qos=long
+#SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1:00:00
 #SBATCH --output=output_for_nf_featurization-%j.out
 
 module load nextflow
@@ -20,14 +20,17 @@ if [ "$HPC_RUN" = "True" ]; then
             --fov_file "patient_well_fov.tsv" \
             --featurize_with_gpu false \
             --conda_env_prefix "/projects/mlippincott@xsede.org/software/anaconda/envs/" \
-            -c ./configs/nextflow_hpc.config
+            -c ./configs/nextflow.config \
+            -profile SLURM_HPC
     else
         nextflow run \
             segmentation_through_featurization.nf \
             --fov_file "patient_well_fov.tsv" \
             --featurize_with_gpu false \
             --conda_env_prefix "/projects/mlippincott@xsede.org/software/anaconda/envs/" \
-            -c ./configs/nextflow_hpc.config
+            -c ./configs/nextflow.config \
+            -profile SLURM_HPC
+
     fi
 else
     if [ "$FEATS_ONLY" = "True" ]; then
@@ -35,8 +38,9 @@ else
             featurization_only.nf \
             --fov_file "patient_well_fov.tsv" \
             --featurize_with_gpu false \
-            -c ./configs/nextflow_local.config \
-            --conda_env_prefix "/home/lippincm/miniforge3/envs/"
+            -c ./configs/nextflow.config \
+            --conda_env_prefix "/home/lippincm/miniforge3/envs/" \
+            -profile local
 
     else
         nextflow run \
@@ -44,6 +48,7 @@ else
             --fov_file "patient_well_fov.tsv" \
             --featurize_with_gpu false \
             --conda_env_prefix "/home/lippincm/miniforge3/envs/" \
-            -c ./configs/nextflow_local.config
+            -c ./configs/nextflow.config \
+            -profile local
     fi
 fi
