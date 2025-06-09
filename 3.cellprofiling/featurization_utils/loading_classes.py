66a2b00d22e1d032588b6635384254780772b468
MikeLippincott <1michaell2017@gmail.com>
2025-06-09 12:34:34 -0600

    run areashape again

--- 3.cellprofiling/featurization_utils/loading_classes.py
+++ 3.cellprofiling/featurization_utils/loading_classes.py
@@ -187,8 +187,11 @@ class ObjectLoader:
         self.label_image = label_image
         self.channel = channel_name
         self.compartment = compartment_name
-        self.objects = skimage.measure.label(label_image)
-        self.object_ids = numpy.unique(self.objects)
+        # get the labeled image objects
+        # this is a 3D image, so the objects are labeled in 3D
+        # self.objects =
+        # self.objects = skimage.measure.label(label_image)
+        self.object_ids = numpy.unique(label_image)
         # drop the 0 label
         self.object_ids = self.object_ids[1:]
 
