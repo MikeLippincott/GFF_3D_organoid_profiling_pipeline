65a83c99e66535f2c4c905a30120fad10b31f442
MikeLippincott <1michaell2017@gmail.com>
2025-06-12 15:47:31 -0600

    update the shell scripts

--- 4.processing_image_based_profiles/merge_features_child.sh
+++ 4.processing_image_based_profiles/merge_features_child.sh
@@ -10,6 +10,8 @@ patient=$2
 cd scripts/ || exit
 
 python 1.merge_feature_parquets.py --well_fov "$well_fov" --patient "$patient"
+python 2.merge_sc.py --well_fov "$well_fov" --patient "$patient"
+python 3.organoid_cell_realationship.py --well_fov "$well_fov" --patient "$patient"
 
 cd ../ || exit
 
