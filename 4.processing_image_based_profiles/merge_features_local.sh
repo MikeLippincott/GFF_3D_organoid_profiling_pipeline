56a4be6f2f9005a5173989c09a5934a7890ea106
MikeLippincott <1michaell2017@gmail.com>
2025-06-12 15:43:42 -0600

    add pycytominer operations

--- 4.processing_image_based_profiles/merge_features_local.sh
+++ 4.processing_image_based_profiles/merge_features_local.sh
@@ -6,7 +6,7 @@ conda activate nf1_image_based_profiling_env
 
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
-patient_array=( "NF0014" "NF0016" "NF0018" "NF0021" "SACRO219" )
+patient_array=( "NF0014" "NF0016" "NF0018" "NF0021" "SARCO219" "SARCO361" )
 
 cd scripts/ || exit
 
@@ -21,9 +21,20 @@ for patient in "${patient_array[@]}"; do
         well_fov=$(basename $dir)
         echo $well_fov
         python 1.merge_feature_parquets.py --well_fov "$well_fov" --patient "$patient"
-    done
-done
+        python 2.merge_sc.py --well_fov "$well_fov" --patient "$patient"
+        python 3.organoid_cell_realationship.py --well_fov "$well_fov" --patient "$patient"
 
+    done
+
+
+    python 4.combining_profiles.py --patient "$patient"
+    python 5.annotation.py --patient "$patient"
+    python 6.normalization.py --patient "$patient"
+    python 7.feature_selection.py --patient "$patient"
+    python 8.aggregation.py --patient "$patient"
+    python 9.merge_consensus_profiles.py--patient "$patient"
+
+done
 
 cd ../ || exit
 
