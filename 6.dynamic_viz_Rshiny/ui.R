21ab8658bf4fd7868e57169bd32090526be7c6d5
MikeLippincott <1michaell2017@gmail.com>
2025-07-29 21:32:54 -0600

    shinyapp v1

---
+++ 6.dynamic_viz_Rshiny/ui.R
@@ -0,0 +1,21 @@
+# Load necessary libraries
+library(shiny)
+
+
+# Define UI
+ui <- fluidPage(
+  titlePanel("UMAP Plot"),
+
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput("sc_or_organoid"),
+      uiOutput("data_level"),
+      uiOutput("PatientSelect"),
+      uiOutput("FacetSelect")
+    ),
+
+    mainPanel(
+      plotOutput("umapPlot")
+    )
+  )
+)
