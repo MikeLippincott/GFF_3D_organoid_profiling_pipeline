d4e2728065317709dc60dfaf70d8e32845dfbcce
MikeLippincott <1michaell2017@gmail.com>
2025-08-03 19:34:28 -0600

    rerun

---
+++ 5.EDA/scripts/11.find_significant_features.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pathlib
+import warnings
+
+import pandas as pd
+import statsmodels.formula.api as smf
+
+warnings.filterwarnings("ignore")  # Ignore all warnings
+warnings.simplefilter("ignore")  # Additional suppression method
+
+try:
+    cfg = get_ipython().config
+    in_notebook = True
+except NameError:
+    in_notebook = False
+if in_notebook:
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm
+# Get the current working directory
+cwd = pathlib.Path.cwd()
+
+if (cwd / ".git").is_dir():
+    root_dir = cwd
+
+else:
+    root_dir = None
+    for parent in cwd.parents:
+        if (parent / ".git").is_dir():
+            root_dir = parent
+            break
+
+# Check if a Git root directory was found
+if root_dir is None:
+    raise FileNotFoundError("No Git root directory found.")
+
+
+# In[2]:
+
+
+profile_dict = {
+    "organoid_fs": {
+        "input_profile_path": pathlib.Path(
+            root_dir, "5.EDA/results/linear_modeling/organoid_fs.parquet"
+        ),
+        "metadata_columns": [
+            "patient",
+            "object_id",
+            "unit",
+            "dose",
+            "treatment",
+            "Target",
+            "Class",
+            "image_set",
+            "Well",
+            "Therapeutic Categories",
+            "single_cell_count",
+        ],
+    },
+    "single_cell_fs": {
+        "input_profile_path": pathlib.Path(
+            root_dir, "5.EDA/results/linear_modeling/sc_fs.parquet"
+        ),
+        "metadata_columns": [
+            "patient",
+            "object_id",
+            "unit",
+            "dose",
+            "treatment",
+            "Target",
+            "Class",
+            "image_set",
+            "Well",
+            "Therapeutic Categories",
+            "parent_organoid",
+        ],
+    },
+}
+
+
+# ## Filter significant features
+# pvalue threshold is set to 0.05 - statistically significant features
+# rsquared threshold is set to 0.5 - the explained variance is at least 50% of the total variance
+# rsquared adjusted threshold is set to positive values - the model performs better than the mean
+#
+
+# In[53]:
+
+
+df = pd.read_parquet(
+    profile_dict["single_cell_fs"]["input_profile_path"],
+)
+print(df.shape)
+
+
+# In[59]:
+
+
+pvalue_threshold_max = 0.05
+rsquared_threshold_min = 0.4
+rsquared_adj_threshold_min = 0
+coefficient_threshold_min = 1
+
+
+# In[60]:
+
+
+# filter significant features
+df_filtered = df[
+    (df["pvalue"] < pvalue_threshold_max)
+    & (df["rsquared"] > rsquared_threshold_min)
+    & (df["rsquared_adj"] > rsquared_adj_threshold_min)
+    & (df["coefficient"].abs() > coefficient_threshold_min)
+].copy()
+print(df_filtered.shape)
+df_filtered.head()
+
+
+# In[61]:
+
+
+df_filtered["treatment"].unique()
+
+
+# In[62]:
+
+
+df_filtered["patient"].unique()
+
+
+# In[63]:
+
+
+df_filtered["feature"].unique()
