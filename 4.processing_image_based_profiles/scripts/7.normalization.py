ce25d5a3d3d26a75d4895e85c77edbf203a73144
MikeLippincott <1michaell2017@gmail.com>
2025-08-08 09:48:26 -0600

    converting notebooks

--- 4.processing_image_based_profiles/scripts/7.normalization.py
+++ 4.processing_image_based_profiles/scripts/7.normalization.py
@@ -4,7 +4,7 @@
 # This notebook performs profile normalization.
 # All profiles are normalized to the DMSO control treated profiles.
 
-# In[1]:
+# In[2]:
 
 
 import argparse
@@ -37,7 +37,7 @@ except NameError:
     in_notebook = False
 
 
-# In[2]:
+# In[3]:
 
 
 if not in_notebook:
@@ -55,7 +55,7 @@ else:
     patient = "NF0014"
 
 
-# In[3]:
+# In[4]:
 
 
 # pathing
@@ -78,7 +78,7 @@ organoid_normalized_output_path = pathlib.Path(
 organoid_normalized_output_path.parent.mkdir(parents=True, exist_ok=True)
 
 
-# In[4]:
+# In[5]:
 
 
 # read in the data
@@ -88,7 +88,7 @@ organoid_annotated_profiles = pd.read_parquet(organoid_annotated_path)
 
 # ### Normalize the single-cell profiles
 
-# In[5]:
+# In[6]:
 
 
 sc_metadata_columns = [
@@ -103,13 +103,16 @@ sc_metadata_columns = [
     "image_set",
     "Well",
     "parent_organoid",
+    "Area.Size.Shape_Cell_CENTER.X",
+    "Area.Size.Shape_Cell_CENTER.Y",
+    "Area.Size.Shape_Cell_CENTER.Z",
 ]
 sc_features_columns = [
     col for col in sc_annotated_profiles.columns if col not in sc_metadata_columns
 ]
 
 
-# In[6]:
+# In[7]:
 
 
 # find inf values and replace with NaN
@@ -118,7 +121,7 @@ sc_annotated_profiles[sc_features_columns] = sc_annotated_profiles[
 ].replace([float("inf"), -float("inf")], np.nan)
 
 
-# In[7]:
+# In[ ]:
 
 
 # normalize the data
@@ -141,7 +144,7 @@ sc_normalized_profiles.head()
 organoid_annotated_profiles.head()
 
 
-# In[9]:
+# In[11]:
 
 
 organoid_metadata_columns = [
@@ -156,6 +159,9 @@ organoid_metadata_columns = [
     "image_set",
     "Well",
     "single_cell_count",
+    "Area.Size.Shape_Organoid_CENTER.X",
+    "Area.Size.Shape_Organoid_CENTER.Y",
+    "Area.Size.Shape_Organoid_CENTER.Z",
 ]
 organoid_features_columns = [
     col
