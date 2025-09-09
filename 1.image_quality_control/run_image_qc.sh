200b7ea6ab04a1c2fc793320a6cbd6e0a1706623
MikeLippincott <1michaell2017@gmail.com>
2025-09-09 09:50:37 -0600

    fix pathing

--- 1.image_quality_control/run_image_qc.sh
+++ 1.image_quality_control/run_image_qc.sh
@@ -13,7 +13,7 @@ conda activate gff_cp_env
 jupyter nbconvert --to script --output-dir="$git_root/1.image_quality_control/nbconverted/" "$git_root/1.image_quality_control/notebooks/*.ipynb"
 
 # run script(s)
-python "$git_root/1.image_quality_control/nbconverted/0.cp_image_qc.py"
+python "$git_root/1.image_quality_control/scripts/0.cp_image_qc.py"
 
 echo "CellProfiler runs have finished!"
 
@@ -23,9 +23,9 @@ conda deactivate
 conda activate gff_preprocessing_env
 
 # run evaluation scripts
-python "$git_root/1.image_quality_control/nbconverted/1.evaluate_blur_qc.py"
-python "$git_root/1.image_quality_control/nbconverted/2.evaluate_saturation_qc.py"
-python "$git_root/1.image_quality_control/nbconverted/4.evaluate_qc_optimization.py"
+python "$git_root/1.image_quality_control/scripts/1.evaluate_blur_qc.py"
+python "$git_root/1.image_quality_control/scripts/2.evaluate_saturation_qc.py"
+python "$git_root/1.image_quality_control/scripts/4.evaluate_qc_optimization.py"
 
 # deactivate the CellProfiler environment
 conda deactivate
@@ -33,7 +33,7 @@ conda deactivate
 conda activate gff_r_env
 
 # run reporting scripts
-Rscript "$git_root/1.image_quality_control/nbconverted/3.generate_qc_report.r"
-Rscript "$git_root/1.image_quality_control/nbconverted/5.generate_qc_optimization_report.r"
+Rscript "$git_root/1.image_quality_control/scripts/3.generate_qc_report.r"
+Rscript "$git_root/1.image_quality_control/scripts/5.generate_qc_optimization_report.r"
 
 echo "Image quality control complete."
